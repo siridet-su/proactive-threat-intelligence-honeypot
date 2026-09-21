34758fa12fdd5d944834f5194636eee9c184792e
siridet-suphab <b6629045@g.sut.ac.th>
2026-09-21 19:31:22 +0700

    chore: remove temporary test and patch scripts

--- patch_workspace.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('dashboard-v2/src/components/filesystem/AuditFilesystemWorkspace.tsx', 'utf8');
-
-// Remove unused imports
-code = code.replace(/import type \{ FilesystemClosedSession, FilesystemTopologySession, FilesystemTopologySnapshot \} from "@\/lib\/dashboardTypes";\n/, '');
-code = code.replace(/import type \{ ForensicTab \} from ".\/FilesystemActivity";\n/, '');
-code = code.replace(/\/\* eslint-disable @typescript-eslint\/no-explicit-any \*\/\n/, '');
-
-fs.writeFileSync('dashboard-v2/src/components/filesystem/AuditFilesystemWorkspace.tsx', code);
